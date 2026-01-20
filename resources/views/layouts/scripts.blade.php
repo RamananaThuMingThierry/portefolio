0390814fd2053b018a6434eb5d0db3ce1b97bb8c
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2026-01-20 09:34:35 +0300

    maj: mettre à jours l'effet du navbar quand on fait une scroll

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,1 +1,13 @@
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}" defer></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const navbar = document.querySelector(".navbar");
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 50) {
+                navbar.classList.add("scrolled");
+            } else {
+                navbar.classList.remove("scrolled");
+            }
+        });
+    });
+</script>
